774cc7f84cdd91f9aa52b5d4892336bb327af34e
Dante1128 <danteyupanqui2811@gmail.com>
2025-05-16 00:47:13 -0400

    categoria

--- src/components/LabItems.jsx
+++ src/components/LabItems.jsx
@@ -1,29 +1,28 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const labItems = [
-  { name: "Balanza Digital", route: "balanza-digital", image: "/image/balanza-digital.png" },
-  { name: "Fuente de poder ", route: "fuente-de-energia", image: "/assets/images/pendulo.jpg" },
-  { name: "Péndulo", route: "pendulo", image: "/assets/images/pendulo.jpg" },
-  { name: "Sensor Ultrasonico", route: "sensor-ultrasonico", image: "/assets/images/balanza.jpg" },
-  { name: "Vernier", route: "vernier", image: "/assets/images/tubo_venturi.jpg" },
-  { name: "Vernier Digital", route: "vernier-digital", image: "/assets/images/tubo_venturi.jpg" },
-  
-  
+  { name: "Balanza Digital", route: "balanza-digital", category: "categoria-1", image: "/image/balanza-digital.png" },
+  { name: "Fuente de poder", route: "fuente-de-energia", category: "categoria-1", image: "/assets/images/pendulo.jpg" },
+  { name: "Péndulo", route: "pendulo", category: "categoria-2", image: "/assets/images/pendulo.jpg" },
+  { name: "Sensor Ultrasonico", route: "sensor-ultrasonico", category: "categoria-3", image: "/assets/images/balanza.jpg" },
+  { name: "Vernier", route: "vernier", category: "categoria-3", image: "/assets/images/tubo_venturi.jpg" },
+  { name: "Vernier Digital", route: "vernier-digital", category: "categoria-3", image: "/assets/images/tubo_venturi.jpg" },
 ];
 
 const LabItems = () => {
+  const { category } = useParams();
   const [search, setSearch] = useState("");
 
-  // Filtrar elementos según la búsqueda
-  const filteredItems = labItems.filter((item) =>
-    item.name.toLowerCase().includes(search.toLowerCase())
+  const filteredItems = labItems.filter(
+    (item) =>
+      item.category === category &&
+      item.name.toLowerCase().includes(search.toLowerCase())
   );
 
   return (
     <div>
-      <h2>ELEMENTOS DE LABORATORIO</h2>
+      <h2>Elementos de laboratorio - {category}</h2>
       <input
         type="text"
         placeholder="Buscar..."
@@ -31,10 +30,12 @@ const LabItems = () => {
         onChange={(e) => setSearch(e.target.value)}
         className="search-input"
       />
-      <div className="lab-container"> {/* Contenedor sin scroll visible */}
+      <div className="lab-container">
         {filteredItems.length > 0 ? (
           filteredItems.map((item, index) => (
             <Link to={`/lab/${item.route}`} key={index} className="lab-item">
+
+
               <div className="lab-card">
                 <img src={item.image} alt={item.name} className="lab-image" />
                 <button>{item.name} ➝</button>
@@ -45,7 +46,6 @@ const LabItems = () => {
           <p>No se encontraron resultados.</p>
         )}
       </div>
-     
     </div>
   );
 };
